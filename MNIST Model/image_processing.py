05ce353467325b8d7e594d282501e38cb8b812d7
Amal Krishna <amalkrishna528@gmail.com>
2023-11-23 21:39:35 +0530

    Made some changes in image cleaning

--- MNIST Model/image_processing.py
+++ MNIST Model/image_processing.py
@@ -2,28 +2,30 @@ import cv2
 import numpy as np
 
 def process_image(frame):
-    # Convert the frame to grayscale
+    #frame to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Apply thresholding
+    # thresholding
     _, thresholded = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY)
 
-    # Apply morphology (closing operation)
+    # Just cleaning the image using dilate
     kernel = np.ones((5, 5), np.uint8)
     dilated = cv2.dilate(thresholded, kernel, iterations=2)
-    # Dilate the image
 
-    # Find contours in the dilated image
+    # Find contours
     contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+    # I need the largest area so finding the max contours
     largest_contour = max(contours, key=cv2.contourArea)
     epsilon = 0.02 * cv2.arcLength(largest_contour, True)
     corners = cv2.approxPolyDP(largest_contour, epsilon, True)
     
-
+    #drawing contours in a blank image
     contour_image = np.zeros_like(frame)
     cv2.drawContours(contour_image, [corners], -1, (255, 255, 255), 2)
 
+
+    #wrapping the sudoku into a new image
     pts1 = np.float32(corners)
     pts2 = np.float32([[0, 0], [0, 450], [450, 450], [450, 0]])
     matrix = cv2.getPerspectiveTransform(pts1, pts2)
@@ -34,21 +36,16 @@ def process_image(frame):
     cv2.imshow('Processed Image', result_image)
 
 # Open the camera
-cap = cv2.VideoCapture(0)  # Use 0 for the default camera, or specify the camera index
-
+cap = cv2.VideoCapture(0)  
 while True:
-    # Read a frame from the camera
     ret, frame = cap.read()
 
-    # Display the frame with an option to process the image
     cv2.imshow('Capture, Process, and Find Contour Corners', frame)
 
-    # Process the captured image when 'c' key is pressed
-    if cv2.waitKey(1) & 0xFF == ord('c'):
+    if cv2.waitKey(1) & 0xFF == ord('c'):#when c is pressed it captures
         process_image(frame)
 
-    # Exit the loop when 'q' key is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):#exits when  is pressed
         break
 
 # Release the camera and close the OpenCV windows
